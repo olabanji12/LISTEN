9b26925a89f5800b2cefb149d187908cc62d397f
olabanji12 <oao190005@utdallas.edu>
2023-10-02 12:27:40 -0500

    modifies the home view to render user display name

--- User/views.py
+++ User/views.py
@@ -2,32 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from spotipy.oauth2 import SpotifyOAuth
-from spotify_auth.utils import is_spotify_authenticated
+from spotify_auth.views import get_user_display_name
+from spotify_auth.utils import is_spotify_authenticated, get_user_tokens
+import spotipy
 
 # Create your views here.
 def home(request):
     is_authenticated  = is_spotify_authenticated(request.session.session_key)
-    print(is_authenticated)
-    return render(request, 'User/home.html', {'is_authenticated': is_authenticated})
-
-# def login_user(request):
-#         if request.method == "POST":
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             user = authenticate(request, username=username, password=password)
-#             if user is not None:
-#                 login(request,user)
-#                 return redirect('home')
-#             else:
-#                 messages.error(request, ("There Was An Error Logging In, Try Again..."))
-#                 return redirect('login')
-#         else:
-#             return render(request, 'registration/login.html')
-
-# def signup(request):
-#     return render(request, 'registration/signup.html' )
-
-# def spotify_login(request):
-#     sp_oauth = SpotifyOAuth(request)
-#     auth_url = sp_oauth.get_authorize_url()
-#     return render(request, 'spotify_login.html', {'auth_url': auth_url})
+    
+    if is_authenticated:
+        user_display_name = get_user_display_name(request)
+        context = {
+            'is_authenticated': is_authenticated,
+            'user_display_name': user_display_name,
+        }
+        return render(request, 'User/home.html', context)
+    else:
+        context = {
+            'is_authenticated': is_authenticated,
+        }
+        return render(request, 'User/home.html', context)
