72344ad7167ce43a5f9f7f2774ff81449abedf68
olabanji12 <oao190005@utdallas.edu>
2023-10-06 16:31:35 -0500

    modifies the model to store user data

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -6,18 +6,12 @@ class UserProfile(models.Model):
     user_id = models.CharField(max_length=255, primary_key = True)
     # Add any additional user-related fields if needed
 
-class Artist(models.Model):
-    artist_name = models.CharField(max_length=255, primary_key = True)
-
-class Album(models.Model):
-    album_title = models.CharField(max_length=255, primary_key = True)
-
 class UserTopArtists(models.Model):
     user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE, to_field = 'user_id')
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, to_field = 'artist_name')
+    artist = models.CharField(max_length=255)
     rank = models.PositiveIntegerField()
 
 class UserTopAlbums(models.Model):
     user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE, to_field = 'user_id')
-    album = models.ForeignKey(Album, on_delete=models.CASCADE, to_field = 'album_title')
+    album = models.CharField(max_length=255)
     rank = models.PositiveIntegerField()
