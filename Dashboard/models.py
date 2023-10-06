bc6f4274547b83c4765bb23ef7c4bab55dec8b7f
olabanji12 <oao190005@utdallas.edu>
2023-10-06 12:26:12 -0500

    creates the DB model to store user dashboard information

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    user_id = models.CharField(max_length=255, primary_key = True)
+    # Add any additional user-related fields if needed
+
+class Artist(models.Model):
+    artist_name = models.CharField(max_length=255, primary_key = True)
+
+class Album(models.Model):
+    album_title = models.CharField(max_length=255, primary_key = True)
+
+class UserTopArtists(models.Model):
+    user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE, to_field = 'user_id')
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, to_field = 'artist_name')
+    rank = models.PositiveIntegerField()
+
+class UserTopAlbums(models.Model):
+    user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE, to_field = 'user_id')
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, to_field = 'album_title')
+    rank = models.PositiveIntegerField()
