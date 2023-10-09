e4f869cd07e358571b02aa02928eaaa6904088ea
olabanji12 <oao190005@utdallas.edu>
2023-10-09 11:57:20 -0500

    queries the data from the DB instead of calling the API for data, filter form is handled

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -2,36 +2,58 @@ import spotipy
 from django.shortcuts import render
 from spotify_auth.utils import is_spotify_authenticated, get_user_tokens
 from User.views import home
-from spotify_auth.views import get_user_display_name
+from spotify_auth.views import get_user_display_name, get_user_dashboard_data, get_user_display_id
+from .forms import FilterNumberSelection
+from .models import UserTopArtists, UserTopTracks, UserProfile
 # Create your views here.
 def user_dashboard(request):
+    form = FilterNumberSelection(request.GET)
     is_authenticated  = is_spotify_authenticated(request.session.session_key)
     user_display_name = get_user_display_name(request)
-   
-    user_tokens = get_user_tokens(request.session.session_key)
-    
-    sp =spotipy.Spotify(user_tokens.access_token)
-    top_artists = sp.current_user_top_artists(limit=10)
-    top_tracks = sp.current_user_top_tracks(limit=10)
-    artists_data = top_artists['items']
-    tracks_data = top_tracks['items']
+    user_id = get_user_display_id(request)
+    # get_user_dashboard_data(request)
 
+    track_records = UserTopTracks.objects.filter(user_id = user_id).order_by('rank').all()
+    artist_records = UserTopArtists.objects.filter(user_id = user_id).order_by('rank').all()
 
     artist_names = []
     artist_image_links = []
     track_names = []
     tracks_image_links = []
 
-    for artist in artists_data:
-        artist_names.append(artist["name"])
-        artist_image_links.append(artist['images'][1]['url'])
+    for records in track_records:
+        track_names.append(records.track)
+        tracks_image_links.append(records.image)
+    
+    for records in artist_records:
+        artist_names.append(records.artist)
+        artist_image_links.append(records.image)
 
+    if form.is_valid():
+        number_filter = int(form.cleaned_data['number'])  # Get the selected number from the form
 
-    for tracks in tracks_data:
-        track_names.append(tracks['name'])
-        tracks_image_links.append(tracks['album']['images'][1]['url'])
-        
-   
+        # Filter your data based on the selected number
+        artist_filtered_names = artist_names[:number_filter]
+        artist_filtered_images = artist_image_links[:number_filter]
+
+        track_filtered_names = track_names[:number_filter]
+        track_filtered_images = tracks_image_links[:number_filter]
+
+        artist_names_and_images = zip(artist_filtered_names,artist_filtered_images)
+        tracks_names_and_images = zip(track_filtered_names,track_filtered_images)
+
+        context = {
+        'is_authenticated': is_authenticated,
+        'artist_names_and_images':artist_names_and_images,
+        'tracks_names_and_images':tracks_names_and_images,
+        'user_display_name': user_display_name,
+        'form': form,
+
+        }
+
+        # Render the template with the filtered data
+        return render(request, "Dashboard/dashboard.html", context)
+    
     artist_names_and_images = zip(artist_names,artist_image_links)
     tracks_names_and_images = zip(track_names,tracks_image_links)
     context = {
@@ -39,5 +61,7 @@ def user_dashboard(request):
         'artist_names_and_images':artist_names_and_images,
         'tracks_names_and_images':tracks_names_and_images,
         'user_display_name': user_display_name,
+        'form': form,
+
     }
     return render(request,"Dashboard/dashboard.html", context)
