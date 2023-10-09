6513470a2bfaf02db97ba5f3f9bfa70c42cd41f8
olabanji12 <oao190005@utdallas.edu>
2023-10-09 10:05:47 -0500

    modifies the UserTopArtists(Tracks) model to prevent duplication of data

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -12,8 +12,14 @@ class UserTopArtists(models.Model):
     image = models.CharField(max_length=255, null=True)
     rank = models.PositiveIntegerField()
 
+    class Meta:
+        unique_together = ('user_id', 'artist', 'image', 'rank')
+
 class UserTopTracks(models.Model):
     user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE, to_field = 'user_id')
     track = models.CharField(max_length=255)
     image = models.CharField(max_length=255, null=True)
     rank = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('user_id', 'track', 'image', 'rank')
