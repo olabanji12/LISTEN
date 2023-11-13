4b86cd8b9509721d961a48f24aa3da5506637d41
olabanji12 <oao190005@utdallas.edu>
2023-11-12 18:52:43 -0600

    creates the db model to store user playlist data

--- Friends/models.py
+++ Friends/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class UserPlaylist(models.Model):
+    user_id = models.CharField(max_length=255, primary_key = True)
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=255, null=True)
+    url = models.CharField(max_length=255)
