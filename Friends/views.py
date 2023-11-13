4fe9453e129d70786d8e94802b750bc6f439a3d8
olabanji12 <oao190005@utdallas.edu>
2023-11-13 12:05:36 -0600

    adds user_id, authentication value, and user_name to the view context variable

--- Friends/views.py
+++ Friends/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from spotify_auth.views import get_user_playlist
-from spotify_auth.views import get_user_display_id
+from spotify_auth.views import get_user_playlist, get_user_display_id, get_user_display_name
+from spotify_auth.utils import is_spotify_authenticated
 from .models import UserPlaylist
 # Create your views here.
 def user_playlist(request):
-    user_id = get_user_display_id()
-    get_user_playlist()
+    user_id = get_user_display_id(request)
+    is_authenticated  = is_spotify_authenticated(request.session.session_key)
+    user_display_name = get_user_display_name(request)
+    get_user_playlist(request)
 
     playlist_records = UserPlaylist.objects.filter(user_id = user_id).all()
 
@@ -18,9 +20,10 @@ def user_playlist(request):
         playlist_image.append(records.image)
         playlist_url.append(records.url)
     
+    playlist_name_image_links = zip(playlist_names, playlist_image, playlist_url)
     context = {
-         'playlist_names': playlist_names,
-         'playlist_image': playlist_image,
-         'playlist_url': playlist_url,
+         'playlist_name_image_links': playlist_name_image_links,
+         'is_authenticated': is_authenticated,
+         'user_display_name': user_display_name,
      }
-    return render(request, 'playlist.html', context)
+    return render(request, 'Friends/playlist.html', context)
