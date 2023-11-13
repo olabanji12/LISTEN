b914104b4fc4564663cc18ef41211c16ee3c28f0
olabanji12 <oao190005@utdallas.edu>
2023-11-13 11:42:50 -0600

    adds the url pattern for playlist/friends app

--- LISTEN/urls.py
+++ LISTEN/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('spotify_auth/', include('spotify_auth.urls')),
     path('spotify/', include('spotify_auth.urls')),
     path('dashboard/', include('Dashboard.urls')),
+    path('playlist/', include('Friends.urls')),
     
 ]
