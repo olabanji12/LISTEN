9f4097aede5627d7738e132e132a89561327f075
olabanji12 <oao190005@utdallas.edu>
2023-11-14 09:44:45 -0600

    sets id primary key, and sets user_id and name as unique values

--- Friends/models.py
+++ Friends/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class UserPlaylist(models.Model):
-    user_id = models.CharField(max_length=255, primary_key = True)
+    id = models.AutoField(primary_key=True)
+    user_id = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     image = models.CharField(max_length=255, null=True)
     url = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ('user_id', 'name')
