97b26418c6ec3a8d58ce8f9ae654d117e7a58fba
olabanji12 <oao190005@utdallas.edu>
2023-11-21 15:56:10 -0600

    checks if objects already exisyt in DB and cnahges media file nameing convention

--- Friends/utils.py
+++ Friends/utils.py
@@ -3,8 +3,8 @@ from io import BytesIO
 from .models import PlaylistQRCode
 from django.core.files import File
 
-def generate_playlist_qrcode(request, url):
-    if not PlaylistQRCode.objects.filter(url=url).exists():
+def generate_playlist_qrcode(request, url, user_id, playlist_name):
+    if not PlaylistQRCode.objects.filter(url=url, user_id = user_id).exists():
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -19,5 +19,5 @@ def generate_playlist_qrcode(request, url):
         image_stream.seek(0)
 
         # Save the image to the database
-        qr_code = PlaylistQRCode(url=url)
-        qr_code.image.save(f'{url}.png', File(image_stream), save=True)
+        qr_code = PlaylistQRCode(url=url, user_id = user_id)
+        qr_code.image.save(f'{playlist_name}.png', File(image_stream), save=True)
