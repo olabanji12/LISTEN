16e3e738a62ef90c5605103f21792eec45bad865
olabanji12 <oao190005@utdallas.edu>
2023-11-21 15:59:12 -0600

    queries the QRcode model and process data for template rendering

--- Friends/views.py
+++ Friends/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from spotify_auth.views import get_user_playlist, get_user_display_id, get_user_display_name
 from spotify_auth.utils import is_spotify_authenticated
-from .models import UserPlaylist
+from .models import UserPlaylist, PlaylistQRCode
 # Create your views here.
 def user_playlist(request):
     user_id = get_user_display_id(request)
@@ -12,17 +12,23 @@ def user_playlist(request):
 
 
     playlist_records = UserPlaylist.objects.filter(user_id = user_id).all()
+    playlist_qrcodes = PlaylistQRCode.objects.filter(user_id = user_id).all()
 
     playlist_names = []
     playlist_image = []
     playlist_url = []
+    playlist_qrcode = []
 
     for records in playlist_records:
         playlist_names.append(records.name)
         playlist_image.append(records.image)
         playlist_url.append(records.url)
     
-    playlist_name_image_links = zip(playlist_names, playlist_image, playlist_url)
+    for qrcode in playlist_qrcodes:
+        playlist_qrcode.append(qrcode.image)
+        print(qrcode.image)
+
+    playlist_name_image_links = zip(playlist_names, playlist_image, playlist_url, playlist_qrcode)
     context = {
          'playlist_name_image_links': playlist_name_image_links,
          'is_authenticated': is_authenticated,
