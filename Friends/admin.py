5c0202f0386a2ad2ed1fe2873f77aef1248eefe4
olabanji12 <oao190005@utdallas.edu>
2023-11-21 15:52:41 -0600

    registers the PlaylistQRCode model

--- Friends/admin.py
+++ Friends/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserPlaylist
+from .models import UserPlaylist, PlaylistQRCode
 
 # Register your models here.
 admin.site.register(UserPlaylist)
+admin.site.register(PlaylistQRCode)
