73dba0c79ee875d811ef7038cb13ca8dab464bc7
olabanji12 <oao190005@utdallas.edu>
2023-12-15 08:42:36 -0600

    modifies the userplaylist model

--- Friends/models.py
+++ Friends/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class UserPlaylist(models.Model):
-    id = models.AutoField(primary_key=True)
     user_id = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     image = models.CharField(max_length=255, null=True)
